d9f96e0de6f0fcb8ede6e506e616d710a0e80254
Alex Wong <awsk1994@gmail.com>
2020-04-17 15:02:35 -0400

    init

---
+++ cs505_hw4/code/test.py
@@ -0,0 +1,43 @@
+from __future__ import division
+import numpy as np
+from hmmlearn import hmm
+
+states = ["Rainy", "Sunny"]
+n_states = len(states)
+
+observations = ["walk", "shop", "clean"]
+n_observations = len(observations)
+
+start_probability = np.array([0.6, 0.4])
+
+transition_probability = np.array([
+  [0.7, 0.3],
+  [0.4, 0.6]
+])
+
+emission_probability = np.array([
+  [0.1, 0.4, 0.5],
+  [0.1, 0.3, 0.6]
+])
+
+model = hmm.MultinomialHMM(n_components=n_states)
+model.startprob=start_probability
+model.transmat=transition_probability
+model.emissionprob=emission_probability
+
+# predict a sequence of hidden states based on visible states
+bob_says = np.array([[0, 2, 1, 1, 2, 0]]).T   # bob_says -> observation
+
+model = model.fit(bob_says)
+logprob, alice_hears = model.decode(bob_says, algorithm="viterbi")	# alice_hears -> state (hidden)
+
+print("bob_says")
+print(bob_says)
+
+print("alice_hears")
+print(alice_hears)
+
+print("Bob says:", ", ".join(map(lambda x: observations[x], bob_says)))
+print("Alice hears:", ", ".join(map(lambda x: states[x], alice_hears)))
+
+
